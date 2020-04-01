97f19d4d39095f667633470d23f401ffb319dfc0
Marcelo Camargo <m.camargo@reply.com>
2020-03-31 22:33:39 -0300

    Adicionando as lojas no home

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -28,4 +28,22 @@
 
         @endforeach
     </div>
+
+    <div class="row">
+        <div class="col-md-12">
+            <h2>Lojas Destaque</h2>
+            <hr>
+        </div>
+        @foreach ($stores as $store)
+        <div class="col-md-4">
+            @if ($store->logo)
+                <img src="{{ asset('storage/' . $store->logo) }}" alt="Logo da Loja {{ $store->name }}" class="img-fluid">
+            @else
+                <img src="https://via.placeholder.com/600X300.png?text=logo" alt="Loja sem logo..." class="img-fluid">
+            @endif
+            <h3>{{ $store->name }}</h3>
+            <p>{{ $store->description }}</p>
+        </div>
+        @endforeach
+    </div>
 @endsection
