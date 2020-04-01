a6c76ce7449d3cff85794ecc26b7ead46caa17f8
Marcelo Camargo <m.camargo@reply.com>
2020-03-31 21:04:29 -0300

    Melhorando o pagamento do pagseguro

--- app/UserOrder.php
+++ app/UserOrder.php
@@ -6,6 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class UserOrder extends Model
 {
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'reference', 'pagseguro_status', 'pagseguro_code', 'store_id', 'items'
+    ];
+
     public function user()
     {
         return $this->belongsTo(User::class);
