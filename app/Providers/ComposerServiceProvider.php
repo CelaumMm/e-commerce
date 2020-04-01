d59a1584ec8d824046afe38a57529722102aeb42
Marcelo Camargo <m.camargo@reply.com>
2020-03-31 23:20:01 -0300

    Compartilhamento da variavel $categories em todas as views com o viewComposer e ServiceProvider

---
+++ app/Providers/ComposerServiceProvider.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class ComposerServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        // $categories = Category::all(['name', 'slug']);
+        
+        // Compartilhamento simple para todas as views
+        // view()->share('categories', $categories);
+
+        // Compartilhamento por função callback
+        // view()->composer('*', function($view) use($categories){
+        //     $view->with('categories', $categories);
+        // });
+
+        // Compartilhamento por arquivo view composer
+        view()->composer('*', 'App\Http\Views\CategoryViewComposer@compose');
+    }
+}
