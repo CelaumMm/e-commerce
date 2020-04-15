259b28171693bad40c715a1f8b1651ff19ab0166
Marcelo Camargo <m.camargo@reply.com>
2020-04-14 21:02:47 -0300

    Criando e-mail de boas vindas para novos usuarios

---
+++ resources/views/emails/user-registered.blade.php
@@ -0,0 +1,7 @@
+<h2>Ola, {{ $user->name }}, Seja Bem-vindo.</h2>
+
+<h3>Obrigado por sua inscrição!</h3>
+
+<p>Faça bom proveito e excelentes compras em nossa loja!</p>
+<hr>
+E-mail enviado em {{ date('d/m/Y H:i:s') }}.
